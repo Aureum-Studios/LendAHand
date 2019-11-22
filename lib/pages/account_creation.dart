cfd2e208648fa123b3a957ea3bdb30add3d8d9a4
Adrian <adrianfrancisco.borrego@gmail.com>
2019-11-21 23:17:42 -0500

    - Removed local AccountService instance - Switched to single widget for entire page instead of maintaining state for children widgets

--- lib/pages/account_creation.dart
+++ lib/pages/account_creation.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:lend_a_hand/services/account_service.dart';
 import 'package:email_validator/email_validator.dart';
 
 class AccountCreation extends StatefulWidget {
@@ -9,15 +8,15 @@ class AccountCreation extends StatefulWidget {
 
 class _AccountCreationState extends State<AccountCreation> {
   final GlobalKey<FormState> _accountKey = new GlobalKey<FormState>();
-  AccountService account = new AccountService();
+  String email;
+  String name;
+  String password;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          'Account Sign up'
-        ),
+        title: Text('Account Sign up'),
         centerTitle: true,
         backgroundColor: Colors.amber,
       ),
@@ -27,143 +26,74 @@ class _AccountCreationState extends State<AccountCreation> {
           autovalidate: true,
           child: Column(
             children: <Widget>[
-              new InputEmail(account: account),
-              new InputName(account: account),
-              new InputPassword(account: account),
-              new SubmitButton(accFormKey: _accountKey, account: account)
+              Container(
+                padding: EdgeInsets.only(left: 40.0, right: 40.0, top: 10.0),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey, width: 1.0)),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey, width: 1.0)),
+                    hintText: 'Ex johndoe@email.com',
+                    labelText: 'EMAIL'),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value) => EmailValidator.validate(value) ? null : 'Please enter a valid email',
+                  onSaved: (value) => email = value,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 40.0, right: 40.0, top: 5.0),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey, width: 1.0)),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey, width: 1.0)),
+                    hintText: 'Ex John Doe',
+                    labelText: 'NAME'),
+                  validator: (value) => value.isNotEmpty ? null : 'Please enter a name',
+                  onSaved: (value) => name = value,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 40.0, right: 40.0, top: 5.0),
+                child: TextFormField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey, width: 1.0)),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey, width: 1.0)),
+                    labelText: 'PASSWORD',
+                  ),
+                  validator: (value) {
+                    if (value.length < 8)
+                      return 'Invalid password, minimum length: 8 characters';
+                    else if (value.length > 16)
+                      return 'Invalid password, maximum length 16 characters';
+                    else
+                      return null;
+                  },
+                  onSaved: (value) => password = value,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 40.0, right: 40.0, top: 5.0),
+                child: RaisedButton(
+                  onPressed: () {
+                    if (_accountKey.currentState.validate()) {
+                      _accountKey.currentState.save();
+
+                      //Call Firebase to create Authenticated User.
+                    }
+                  },
+                  color: Colors.amber,
+                  child: Text('Sign Up'),
+                ),
+              )
             ],
           ),
         ),
-      ),
-    );
-  }
-}
-
-class SubmitButton extends StatelessWidget {
-  const SubmitButton({
-    Key key,
-    @required GlobalKey<FormState> accFormKey,
-    @required this.account,
-  }) : _accFormKey = accFormKey, super(key: key);
-
-  final GlobalKey<FormState> _accFormKey;
-  final AccountService account;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(left: 40.0, right: 40.0, top: 5.0),
-      child: RaisedButton(
-        onPressed: () {
-          if(_accFormKey.currentState.validate()) {
-            _accFormKey.currentState.save();
-
-            print(this.account.name);
-            print(this.account.email);
-            print(this.account.password);
-            account.saveAccount();
-          }
-        },
-        color: Colors.amber,
-        child: Text('Sign Up'),
-      ),
-    );
-  }
-}
-
-class InputPassword extends StatelessWidget {
-  const InputPassword({
-    Key key,
-    @required this.account,
-  }) : super(key: key);
-
-  final AccountService account;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(left: 40.0, right: 40.0, top: 5.0),
-      child: TextFormField(
-        obscureText: true,
-        decoration: const InputDecoration(
-          focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.grey, width: 1.0)
-          ),
-          enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.grey, width: 1.0)
-          ),
-          labelText: 'PASSWORD',
-        ),
-        validator: (value) {
-          if(value.length < 8)
-            return 'Invalid password, minimum length: 8 characters';
-          else if(value.length > 16)
-            return 'Invalid password, maximum length 16 characters';
-          else
-            return null;
-        },
-        onSaved: (value) => account.password = value,
-      ),
-    );
-  }
-}
-
-class InputName extends StatelessWidget {
-  const InputName({
-    Key key,
-    @required this.account,
-  }) : super(key: key);
-
-  final AccountService account;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(left: 40.0, right: 40.0, top: 5.0),
-      child: TextFormField(
-        decoration: const InputDecoration(
-          focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.grey, width: 1.0)
-          ),
-          enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.grey, width: 1.0)
-          ),
-          hintText: 'Ex John Doe',
-          labelText: 'NAME'
-        ),
-        validator: (value) => value.isNotEmpty ? null : 'Please enter a name',
-        onSaved: (value) => account.name = value,
-      ),
-    );
-  }
-}
-
-class InputEmail extends StatelessWidget {
-  const InputEmail({
-    Key key,
-    @required this.account,
-  }) : super(key: key);
-
-  final AccountService account;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(left: 40.0, right: 40.0, top: 10.0),
-      child: TextFormField(
-        decoration: const InputDecoration(
-          focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.grey, width: 1.0)
-          ),
-          enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.grey, width: 1.0)
-          ),
-          hintText: 'Ex johndoe@email.com',
-          labelText: 'EMAIL'
-        ),
-        keyboardType: TextInputType.emailAddress,
-        validator: (value) => EmailValidator.validate(value) ? null : 'Please enter a valid email',
-        onSaved: (value) => account.email = value,
       ),
     );
   }
