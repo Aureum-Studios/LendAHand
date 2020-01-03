2a4dff0ddb43ef535b776345c1c3ffcd7bb170f5
croxx219 <osniel219@gmail.com>
2020-01-03 17:35:59 -0500

    Added landing page as master of all other pages.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:lend_a_hand/pages/jobs_page.dart';
+import 'package:lend_a_hand/pages/loading_account.dart';
 import 'package:lend_a_hand/services/auth_service.dart';
 import 'package:provider/provider.dart';
 
-//TODO: Make a navigation page that can be used to navigate based on tabs
+
 class HomePage extends StatefulWidget {
-//  final FirebaseUser currentUser;
   HomePage();
 
   @override
@@ -15,6 +16,16 @@ class _HomePageState extends State<HomePage> {
   final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
   new GlobalKey<RefreshIndicatorState>();
   int _currentIndex = 0;
+
+  final PageStorageBucket bucket = PageStorageBucket();
+
+  //TODO: Replace for rest of pages
+  List<Widget> _pages = [
+    JobsPage(key: PageStorageKey('JobsPage')),
+    LoadingAccount(),
+    JobsPage(key: PageStorageKey('JobsPage2')),
+    JobsPage(key: PageStorageKey('JobsPage3')),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -51,44 +62,9 @@ class _HomePageState extends State<HomePage> {
             ]),
         body: RefreshIndicator(
           key: _refreshIndicatorKey,
-          child: GridView.count(
-            primary: false,
-            padding: const EdgeInsets.all(20),
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-            crossAxisCount: 2,
-            children: <Widget>[
-              Container(
-                padding: const EdgeInsets.all(8),
-                child: const Text('He\'d have you all unravel at the'),
-                color: Colors.teal[100],
-              ),
-              Container(
-                padding: const EdgeInsets.all(8),
-                child: const Text('Heed not the rabble'),
-                color: Colors.teal[200],
-              ),
-              Container(
-                padding: const EdgeInsets.all(8),
-                child: const Text('Sound of screams but the'),
-                color: Colors.teal[300],
-              ),
-              Container(
-                padding: const EdgeInsets.all(8),
-                child: const Text('Who scream'),
-                color: Colors.teal[400],
-              ),
-              Container(
-                padding: const EdgeInsets.all(8),
-                child: const Text('Revolution is coming...'),
-                color: Colors.teal[500],
-              ),
-              Container(
-                padding: const EdgeInsets.all(8),
-                child: const Text('Revolution, they...'),
-                color: Colors.teal[600],
-              ),
-            ],
+          child: PageStorage(
+            child: _pages[_currentIndex],
+            bucket: bucket,
           ),
           onRefresh: _handleRefresh,
         ));
