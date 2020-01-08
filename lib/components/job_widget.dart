d0ad300d4c41d571ff5d8b6f27e440625e206b76
croxx219 <osniel219@gmail.com>
2020-01-07 22:02:19 -0500

    Changed GridView to ListView for UX and performance improvements. Refactored some classes/packages.

---
+++ lib/components/job_widget.dart
@@ -0,0 +1,45 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class JobWidget extends StatelessWidget {
+  final String requesterName;
+  final String description;
+  final int phoneNumber;
+  final GeoPoint location;
+  final Timestamp datePosted;
+
+  DateFormat _format = new DateFormat('hh:mm - MM/dd/yy');
+
+  // If the widget was to change to Stateful, then duplicate in State class
+  // with private fields
+  JobWidget(
+      {@required this.requesterName,
+      @required this.datePosted,
+      @required this.description,
+      this.phoneNumber,
+      this.location});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      child: InkWell(
+        splashColor: Colors.blue.withAlpha(30),
+        onTap: () {
+          print(
+              'This is the location ${this.location.latitude} + ${this.location.latitude}');
+        },
+        child: Column(mainAxisSize: MainAxisSize.min, children: [
+          ListTile(
+            title: Text(this.requesterName),
+            subtitle: Text(this.description),
+            trailing: Text(_format.format(this.datePosted.toDate())),
+          ),
+//                  Text(this.description),
+//              Text(this.datePosted.toString())
+        ]),
+      ),
+    );
+  }
+}
--- lib/pages/jobs_page.dart
+++ lib/pages/jobs_page.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:lend_a_hand/components/job_widget.dart';
 
 class JobsPage extends StatefulWidget {
   JobsPage({Key key}) : super(key: key);
@@ -18,12 +19,18 @@ class _JobsPageState extends State<JobsPage> {
           if (!snapshot.hasData) {
             return Center(child: const Text('Loading events...'));
           }
-          return GridView.builder(
+          return ListView.builder(
+              padding: const EdgeInsets.all(8),
               itemCount: snapshot.data.documents.length,
-              gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3),
               itemBuilder: (context, index) {
-                return Text(snapshot.data.documents[index]['description']);
+//                return Text(snapshot.data.documents[index]['description']);
+                var collection = snapshot.data.documents[index];
+                return JobWidget(
+                  datePosted: collection['date_posted'],
+                  description: collection['description'],
+                  requesterName: collection['name'],
+                  location: collection['location'],
+                  phoneNumber: collection['phone_number'],);
               });
         });
   }
